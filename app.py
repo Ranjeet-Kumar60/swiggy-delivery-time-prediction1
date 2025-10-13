ee336431ba3a6988640113ed57a72131d58f3f95
ranjeet-kumar60 <ranjeetdhn60@gmail.com>
2025-10-13 05:13:25 -0700

    app working checked by taking sample raw data

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ import json
 import joblib
 from mlflow import MlflowClient
 from sklearn import set_config
+from scripts.data_clean_utils import perform_data_cleaning
 
-
+# set the output as pandas
 set_config(transform_output='pandas')
 
+# initialize dagshub
 import dagshub
 import mlflow.client
 
@@ -20,30 +22,33 @@ dagshub.init(repo_owner='Ranjeet-Kumar60', repo_name='swiggy-delivery-time-predi
 mlflow.set_tracking_uri('https://dagshub.com/Ranjeet-Kumar60/swiggy-delivery-time-prediction1.mlflow')
 
 
+class Data(BaseModel):  
+    ID: str
+    Delivery_person_ID: str
+    Delivery_person_Age: str
+    Delivery_person_Ratings: str
+    Restaurant_latitude: float
+    Restaurant_longitude: float
+    Delivery_location_latitude: float
+    Delivery_location_longitude: float
+    Order_Date: str
+    Time_Orderd: str
+    Time_Order_picked: str
+    Weatherconditions: str
+    Road_traffic_density: str
+    Vehicle_condition: int
+    Type_of_order: str
+    Type_of_vehicle: str
+    multiple_deliveries: str
+    Festival: str
+    City: str
 
-
-
-class Data(BaseModel):
-    age: float
-    ratings: float
-    weather: str
-    traffic: str
-    vehicle_condition: int
-    type_of_order: str
-    type_of_vehicle: str
-    multiple_deliveries: float
-    festival: str
-    city_type: str
-    is_weekend: int
-    pickup_time_minutes: float
-    order_time_of_day: str
-    distance: float
-    distance_type: str
-
-
+    
+    
 def load_model_information(file_path):
     with open(file_path) as f:
         run_info = json.load(f)
+        
     return run_info
 
 
@@ -53,77 +58,88 @@ def load_transformer(transformer_path):
 
 
 
-num_cols = ["age", "ratings", "pickup_time_minutes", "distance"]
+# columns to preprocess in data
+num_cols = ["age",
+            "ratings",
+            "pickup_time_minutes",
+            "distance"]
 
-nominal_cat_cols = [
-    'weather',
-    'type_of_order',
-    'type_of_vehicle',
-    "festival",
-    "city_type",
-    "is_weekend",
-    "order_time_of_day"
-]
+nominal_cat_cols = ['weather',
+                    'type_of_order',
+                    'type_of_vehicle',
+                    "festival",
+                    "city_type",
+                    "is_weekend",
+                    "order_time_of_day"]
 
-ordinal_cat_cols = ["traffic", "distance_type"]
+ordinal_cat_cols = ["traffic","distance_type"]
 
-
-
+#mlflow client
 client = MlflowClient()
 
+# load the model info to get the model name
 model_name = load_model_information("run_information.json")['model_name']
 
+# stage of the model
 stage = "Staging"
 
 
-latest_model_ver = client.get_latest_versions(name=model_name, stages=[stage])
-print(f"Latest model in staging: version {latest_model_ver[0].version}")
-
-
+# load model path
 model_path = f"models:/{model_name}/{stage}"
 
+# load the latest model from model registry
 model = mlflow.sklearn.load_model(model_path)
 
+# load the preprocessor
 preprocessor_path = "models/preprocessor.joblib"
 preprocessor = load_transformer(preprocessor_path)
 
+# build the model pipeline
 model_pipe = Pipeline(steps=[
-    ('preprocess', preprocessor),
-    ('regressor', model)
+    ('preprocess',preprocessor),
+    ("regressor",model)
 ])
 
-
-
+# create the app
 app = FastAPI()
 
-@app.get("/")
+# create the home endpoint
+@app.get(path="/")
 def home():
-    return "Welcome to the Swiggy Food Delivery Time Prediction App (Cleaned Data Version)"
+    return "Welcome to the Swiggy Food Delivery Time Prediction App"
 
-@app.post("/predict")
+# create the predict endpoint
+@app.post(path="/predict")
 def do_predictions(data: Data):
     pred_data = pd.DataFrame({
-        'age': data.age,
-        'ratings': data.ratings,
-        'weather': data.weather,
-        'traffic': data.traffic,
-        'vehicle_condition': data.vehicle_condition,
-        'type_of_order': data.type_of_order,
-        'type_of_vehicle': data.type_of_vehicle,
+        'ID': data.ID,
+        'Delivery_person_ID': data.Delivery_person_ID,
+        'Delivery_person_Age': data.Delivery_person_Age,
+        'Delivery_person_Ratings': data.Delivery_person_Ratings,
+        'Restaurant_latitude': data.Restaurant_latitude,
+        'Restaurant_longitude': data.Restaurant_longitude,
+        'Delivery_location_latitude': data.Delivery_location_latitude,
+        'Delivery_location_longitude': data.Delivery_location_longitude,
+        'Order_Date': data.Order_Date,
+        'Time_Orderd': data.Time_Orderd,
+        'Time_Order_picked': data.Time_Order_picked,
+        'Weatherconditions': data.Weatherconditions,
+        'Road_traffic_density': data.Road_traffic_density,
+        'Vehicle_condition': data.Vehicle_condition,
+        'Type_of_order': data.Type_of_order,
+        'Type_of_vehicle': data.Type_of_vehicle,
         'multiple_deliveries': data.multiple_deliveries,
-        'festival': data.festival,
-        'city_type': data.city_type,
-        'is_weekend': data.is_weekend,
-        'pickup_time_minutes': data.pickup_time_minutes,
-        'order_time_of_day': data.order_time_of_day,
-        'distance': data.distance,
-        'distance_type': data.distance_type
-    },index=[0])
+        'Festival': data.Festival,
+        'City': data.City
+        },index=[0]
+    )
+    # clean the raw input data
+    cleaned_data = perform_data_cleaning(pred_data)
+    # get the predictions
+    predictions = model_pipe.predict(cleaned_data)[0]
 
-    prediction = model_pipe.predict(pred_data)[0]
-
-    return prediction
-
-
+    return predictions
+   
+   
 if __name__ == "__main__":
-    uvicorn.run(app="app:app")
+    uvicorn.run(app="app:app",host="0.0.0.0",port=8000)
