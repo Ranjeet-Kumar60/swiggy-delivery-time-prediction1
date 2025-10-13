2ec3604db1a71ba39002c070d3e7c9837721c7f9
ranjeet-kumar60 <ranjeetdhn60@gmail.com>
2025-10-12 21:39:06 -0700

    app working tested with cleaned data

---
+++ app.py
@@ -0,0 +1,129 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from sklearn.pipeline import Pipeline
+import uvicorn
+import pandas as pd
+import mlflow
+import json
+import joblib
+from mlflow import MlflowClient
+from sklearn import set_config
+
+
+set_config(transform_output='pandas')
+
+import dagshub
+import mlflow.client
+
+dagshub.init(repo_owner='Ranjeet-Kumar60', repo_name='swiggy-delivery-time-prediction1', mlflow=True)
+
+mlflow.set_tracking_uri('https://dagshub.com/Ranjeet-Kumar60/swiggy-delivery-time-prediction1.mlflow')
+
+
+
+
+
+class Data(BaseModel):
+    age: float
+    ratings: float
+    weather: str
+    traffic: str
+    vehicle_condition: int
+    type_of_order: str
+    type_of_vehicle: str
+    multiple_deliveries: float
+    festival: str
+    city_type: str
+    is_weekend: int
+    pickup_time_minutes: float
+    order_time_of_day: str
+    distance: float
+    distance_type: str
+
+
+def load_model_information(file_path):
+    with open(file_path) as f:
+        run_info = json.load(f)
+    return run_info
+
+
+def load_transformer(transformer_path):
+    transformer = joblib.load(transformer_path)
+    return transformer
+
+
+
+num_cols = ["age", "ratings", "pickup_time_minutes", "distance"]
+
+nominal_cat_cols = [
+    'weather',
+    'type_of_order',
+    'type_of_vehicle',
+    "festival",
+    "city_type",
+    "is_weekend",
+    "order_time_of_day"
+]
+
+ordinal_cat_cols = ["traffic", "distance_type"]
+
+
+
+client = MlflowClient()
+
+model_name = load_model_information("run_information.json")['model_name']
+
+stage = "Staging"
+
+
+latest_model_ver = client.get_latest_versions(name=model_name, stages=[stage])
+print(f"Latest model in staging: version {latest_model_ver[0].version}")
+
+
+model_path = f"models:/{model_name}/{stage}"
+
+model = mlflow.sklearn.load_model(model_path)
+
+preprocessor_path = "models/preprocessor.joblib"
+preprocessor = load_transformer(preprocessor_path)
+
+model_pipe = Pipeline(steps=[
+    ('preprocess', preprocessor),
+    ('regressor', model)
+])
+
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Welcome to the Swiggy Food Delivery Time Prediction App (Cleaned Data Version)"
+
+@app.post("/predict")
+def do_predictions(data: Data):
+    pred_data = pd.DataFrame({
+        'age': data.age,
+        'ratings': data.ratings,
+        'weather': data.weather,
+        'traffic': data.traffic,
+        'vehicle_condition': data.vehicle_condition,
+        'type_of_order': data.type_of_order,
+        'type_of_vehicle': data.type_of_vehicle,
+        'multiple_deliveries': data.multiple_deliveries,
+        'festival': data.festival,
+        'city_type': data.city_type,
+        'is_weekend': data.is_weekend,
+        'pickup_time_minutes': data.pickup_time_minutes,
+        'order_time_of_day': data.order_time_of_day,
+        'distance': data.distance,
+        'distance_type': data.distance_type
+    },index=[0])
+
+    prediction = model_pipe.predict(pred_data)[0]
+
+    return prediction
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="app:app")
